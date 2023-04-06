4f4b5baf989fc22a69769359d35538cf32fa7cb8
tadashikanizawa <tadashikanizawa.fk@gmail.com>
2023-04-06 18:46:12 +0900

    add o chatpgt

--- Krisiun Project/G-Code/Kijun.cs
+++ Krisiun Project/G-Code/Kijun.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,16 +25,93 @@ namespace Krisiun_Project.Ferramentas.Endmills
         public float KijunRd { get; set; }
         public int KijunShiage { get; set; }
 
+        //O do chatgpt
+        public static StringBuilder GerarGCode(float diametroPeca, float diametroEndmill, float angulo, float profundidadePorPasso, int passagensAcabamento, float margemSeguranca)
+        {
+            float raioPeca = diametroPeca / 2;
+            float raioEndmill = diametroEndmill / 2;
+            double anguloRadianos = angulo * Math.PI / 180;
 
-        public StringBuilder kijunkakou(Kijunmen kijunmen, Pitch_principal.Peca peca, RadioButton front, RadioButton back, RadioButton right, RadioButton left)
+            StringBuilder gcode = new StringBuilder();
+
+            float profundidadeAtual = profundidadePorPasso;
+
+            while (profundidadeAtual <= raioPeca)
+            {
+                float raioEfetivo = raioPeca + raioEndmill - profundidadeAtual;
+                float x = raioEfetivo * (float)Math.Cos(anguloRadianos);
+                float y = raioEfetivo * (float)Math.Sin(anguloRadianos);
+
+                gcode.AppendLine($"G01 X{x + margemSeguranca} Y0 F100"); // Movimento inicial com margem de segurança
+                gcode.AppendLine($"G01 X{x} Y0 F100"); // Movimento inicial
+                gcode.AppendLine($"G01 X{x} Y{y} F100"); // Movimento até o topo
+                gcode.AppendLine($"G01 X{x} Y{-y} F100"); // Movimento até a base
+                gcode.AppendLine($"G01 X{x + margemSeguranca} Y{-y} F100"); // Movimento final com margem de segurança
+
+                profundidadeAtual += profundidadePorPasso;
+            }
+
+            // Passagens de acabamento
+            for (int i = 0; i < passagensAcabamento; i++)
+            {
+                float raioEfetivo = raioPeca + raioEndmill - raioPeca;
+                float x = raioEfetivo * (float)Math.Cos(anguloRadianos);
+                float y = raioEfetivo * (float)Math.Sin(anguloRadianos);
+
+                gcode.AppendLine($"G01 X{x + margemSeguranca} Y0 F100"); // Movimento inicial com margem de segurança
+                gcode.AppendLine($"G01 X{x} Y0 F100"); // Movimento inicial
+                gcode.AppendLine($"G01 X{x} Y{y} F100"); // Movimento até o topo
+                gcode.AppendLine($"G01 X{x} Y{-y} F100"); // Movimento até a base
+                gcode.AppendLine($"G01 X{x + margemSeguranca} Y{-y} F100"); // Movimento final com margem de segurança
+            }
+
+            return gcode;
+        }
+        public static List<PointF> CalcularTrajetoria(float diametroPeca, float diametroEndmill, float angulo)
+        {
+            float raioPeca = diametroPeca / 2;
+            float raioEndmill = diametroEndmill / 2;
+            float raioEfetivo = raioPeca + raioEndmill - 1; // Subtraia 1 mm para a fresagem
+
+            double anguloRadianos = angulo * Math.PI / 180;
+
+            float x = raioEfetivo * (float)Math.Cos(anguloRadianos);
+            float y = raioEfetivo * (float)Math.Sin(anguloRadianos);
+
+            List<PointF> pontos = new List<PointF>
+        {
+            new PointF(x, y),
+            new PointF(x, -y)
+        };
+
+            return pontos;
+        }
+        public void DesenharTrajetoria(Graphics g, List<PointF> trajetoria, float offsetX, float offsetY)
+        {
+            // Ajusta a escala da trajetória
+            float escala = 3;
+
+            // Desenha a trajetória
+            using (Pen pen = new Pen(Color.Black, 2))
+            {
+                for (int i = 0; i < trajetoria.Count - 1; i++)
+                {
+                    PointF pontoInicial = new PointF(trajetoria[i].X * escala + offsetX, trajetoria[i].Y * escala + offsetY);
+                    PointF pontoFinal = new PointF(trajetoria[i + 1].X * escala + offsetX, trajetoria[i + 1].Y * escala + offsetY);
+                    g.DrawLine(pen, pontoInicial, pontoFinal);
+                }
+            }
+        }
+        //O meu
+        public static StringBuilder kijunkakou(Kijunmen kijunmen, Pitch_principal.Peca peca, RadioButton front, RadioButton back, RadioButton right, RadioButton left)
         {
             StringBuilder kijun_code = new StringBuilder();
             float haba = kijunmen.KijunHaba/ 2;
             float anzen = kijunmen.KijunAnzen;
-            float kei = kijunmen.Kijunkei / 2;
+            float kei = kijunmen.KijunKei / 2;
             float y = kijunmen.KijunHaba;
             float z = kijunmen.KijunZ;
-            float okuri = kijunmen.Kijunokuri;
+            float okuri = kijunmen.KijunOkuri;
             float rb = kijunmen.KijunRd;
             float seihin = peca.height / 2;
             float pontodepartidax = haba + anzen + kei;
@@ -179,11 +257,8 @@ namespace Krisiun_Project.Ferramentas.Endmills
 
 
 
-
-                return kijun_code.ToString();
-
-
             }
+            return kijun_code;
         }
     }
 }
